f8b210e030bbe82e658c0d98eeac267ad114772b
Caleb Curry <calebcurry@Calebs-iMac.local>
2020-11-12 08:24:38 -0500

    run the app (also with debug)

--- application.py
+++ application.py
@@ -5,3 +5,8 @@ application = Flask(__name__)
 @application.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
