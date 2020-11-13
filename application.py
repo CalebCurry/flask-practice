01fa6374208546b2b0e0f0921ef75305a02dbbb7
Caleb Curry <calebcurry@Calebs-iMac.local>
2020-11-13 15:45:11 -0500

    404 added and starting to practice with db)

--- application.py
+++ application.py
@@ -1,8 +1,20 @@
-from flask import Flask
-from flask import jsonify
-from flask import render_template
+from flask import Flask, jsonify, render_template, abort
+from flask_sqlalchemy import SQLAlchemy
+
 application = Flask(__name__)
 app = application
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+
+class Drink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    rating = db.Column(db.Integer, nullable=False, default=0)
+
+    def __repr__(self):
+        return f"{self.name} is rated {self.rating}"
+
 
 drinks = {
     0: {
@@ -14,6 +26,11 @@ drinks = {
         'rating': 10
     }
 }
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 @app.route('/')
@@ -31,4 +48,8 @@ def get_drinks():
 def get_rating(id):
     # this needs to be an int.
     # return drinks.get(int(id)) # for json
+    drink = drinks.get(int(id))
+    if drink is None:
+        abort(404, description="Resource not found")
+
     return render_template("drink.html", drink=drinks.get(int(id)))
