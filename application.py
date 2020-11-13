f2ac40be839828db49b23c47eb36a3e7851d348b
Caleb Curry <calebcurry@Calebs-iMac.local>
2020-11-13 08:51:26 -0500

    drink templates

--- application.py
+++ application.py
@@ -1,15 +1,34 @@
 from flask import Flask
+from flask import jsonify
+from flask import render_template
 application = Flask(__name__)
 app = application
+
+drinks = {
+    0: {
+        'name': 'Black Cherry',
+        'rating': 0
+    },
+    1: {
+        'name': 'Grape',
+        'rating': 10
+    }
+}
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return "hello World"
 
 
-"""
-if __name__ == "__main__":
-    application.debug = True
-    application.run()
-"""
+@app.route('/drinks')
+def get_drinks():
+    # return jsonify({'drinks': drinks})
+    return render_template("drinks.html", drinks=drinks)
+
+
+@app.route('/drinks/<id>')
+def get_rating(id):
+    # this needs to be an int.
+    # return drinks.get(int(id)) # for json
+    return render_template("drink.html", drink=drinks.get(int(id)))
---
+++ test.py
@@ -0,0 +1,12 @@
+drinks = {
+    0: {
+        'name': 'Black Cherry',
+        'rating': 0
+    },
+    1: {
+        'name': 'Grape',
+        'rating': 10
+    }
+}
+
+print(drinks.get(1))
